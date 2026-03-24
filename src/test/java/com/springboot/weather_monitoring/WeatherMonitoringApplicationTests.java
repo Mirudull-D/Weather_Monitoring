7ebbffbe5be70660f24116b1c5ab97182e4a3184
Mirudull-D <d.mirudull@gmail.com>
2026-03-24 19:52:08 +0530

    initial commit

---
+++ src/test/java/com/springboot/weather_monitoring/WeatherMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.weather_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
