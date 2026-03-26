90a21d2dbb72f7acc7eed92659161626c7791d78
Mirudull-D <d.mirudull@gmail.com>
2026-03-26 23:52:44 +0530

    Fix

--- src/main/java/com/springboot/weather_monitoring/repositories/DailySummaryRepository.java
+++ src/main/java/com/springboot/weather_monitoring/repositories/DailySummaryRepository.java
@@ -1,10 +1,11 @@
 package com.springboot.weather_monitoring.repositories;
 
+import com.springboot.weather_monitoring.domains.entities.DailySummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface DailySummaryRepository extends JpaRepository<DailySummaryRepository, UUID> {
+public interface DailySummaryRepository extends JpaRepository<DailySummary, UUID> {
 }
